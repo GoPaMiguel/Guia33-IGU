3c304714a0146808bae7721ddfaf7e1e70360963
MiguelDev <migopa0222@gmail.com>
2024-04-25 22:44:28 -0500

    GUIA 33 Terminada

---
+++ Guia-GUI-33-main/Reutilizar/src/elementosventana/BotonContador.java
@@ -0,0 +1,50 @@
+
+package elementosventana;
+
+import paquetePrincipal.*;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author migop
+ */
+public class BotonContador extends JButton {
+    
+int pulsaciones;
+
+public BotonContador(){
+pulsaciones = 0;
+}
+
+public void setPulsaciones(int p){
+pulsaciones = p;
+}
+
+public int getPulsaciones(){
+return pulsaciones;
+}
+
+public void incrementa(){
+pulsaciones++;
+}
+
+public void decrementa(){
+pulsaciones--;
+}
+
+public void reiniciar(){
+pulsaciones = 0;
+}
+
+public void aumenta(int c){
+pulsaciones = pulsaciones + c;
+}
+
+public void disminuye(int c){
+pulsaciones = pulsaciones - c;
+}
+
+
+    
+    
+}
